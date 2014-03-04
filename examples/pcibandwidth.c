26f0c09c1de1a3351ed5acd1ef97bcf97df7e0d3
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-03-04 17:36:28 +0100

    Loop num_runs times inside the measure function

--- examples/pcibandwidth.c
+++ examples/pcibandwidth.c
@@ -6,10 +6,8 @@ typedef struct {
     cl_context context;
     cl_command_queue queue;
     cl_kernel kernel;
-    GTimer *upload;
-    GTimer *download;
+    guint num_runs;
 } App;
-
 
 static const char* source =
     "__kernel void touch(global char *array) "
@@ -19,73 +17,90 @@ static const char* source =
 
 
 void
-measure_transfer_normal (App *app, size_t size)
+measure_transfer_normal (App *app, size_t size, gdouble *upload, gdouble *download)
 {
     cl_int errcode;
     cl_mem buffer;
     char *array;
+    GTimer *timer;
 
+    timer = g_timer_new ();
     buffer = clCreateBuffer (app->context, CL_MEM_READ_WRITE, size, NULL, &errcode);
     OCL_CHECK_ERROR (errcode);
 
     array = g_malloc0 (size);
 
-    g_timer_start (app->upload);
-    OCL_CHECK_ERROR (clEnqueueWriteBuffer (app->queue, buffer, CL_TRUE, 0, size, array, 0, NULL, NULL));
-    g_timer_stop (app->upload);
+    for (guint i = 0; i < app->num_runs; i++) {
+        g_timer_start (timer);
+        OCL_CHECK_ERROR (clEnqueueWriteBuffer (app->queue, buffer, CL_TRUE, 0, size, array, 0, NULL, NULL));
+        g_timer_stop (timer);
 
-    /* Call a stupid kernel to avoid any optimizing assumptions of the OpenCL
-     * run-time */
+        *upload += g_timer_elapsed (timer, NULL);
 
-    OCL_CHECK_ERROR (clSetKernelArg (app->kernel, 0, sizeof (cl_mem), &buffer));
-    OCL_CHECK_ERROR (clEnqueueNDRangeKernel (app->queue, app->kernel, 1, NULL, &size, NULL, 0, NULL, NULL));
+        /* Call a stupid kernel to avoid any optimizing assumptions of the OpenCL
+         * run-time */
 
-    g_timer_start (app->download);
-    OCL_CHECK_ERROR (clEnqueueReadBuffer (app->queue, buffer, CL_TRUE, 0, size, array, 0, NULL, NULL));
-    g_timer_stop (app->download);
+        OCL_CHECK_ERROR (clSetKernelArg (app->kernel, 0, sizeof (cl_mem), &buffer));
+        OCL_CHECK_ERROR (clEnqueueNDRangeKernel (app->queue, app->kernel, 1, NULL, &size, NULL, 0, NULL, NULL));
+
+        g_timer_start (timer);
+        OCL_CHECK_ERROR (clEnqueueReadBuffer (app->queue, buffer, CL_TRUE, 0, size, array, 0, NULL, NULL));
+        g_timer_stop (timer);
+
+        *download += g_timer_elapsed (timer, NULL);
+    }
 
     g_free (array);
     clReleaseMemObject (buffer);
+    g_timer_destroy (timer);
 }
 
 void
-measure_transfer_pinned (App *app, size_t size)
+measure_transfer_pinned (App *app, size_t size, gdouble *upload, gdouble *download)
 {
     cl_int errcode;
     cl_mem buffer;
     cl_event event;
     char *array;
+    GTimer *timer;
 
+    timer = g_timer_new ();
     buffer = clCreateBuffer (app->context, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR, size, NULL, &errcode);
     OCL_CHECK_ERROR (errcode);
 
-    g_timer_start (app->upload);
-    array = clEnqueueMapBuffer (app->queue, buffer, CL_TRUE, CL_MAP_WRITE, 0, size, 0, NULL, NULL, &errcode);
-    OCL_CHECK_ERROR (errcode);
+    for (guint i = 0; i < app->num_runs; i++) {
+        g_timer_start (timer);
+        array = clEnqueueMapBuffer (app->queue, buffer, CL_TRUE, CL_MAP_WRITE, 0, size, 0, NULL, NULL, &errcode);
+        OCL_CHECK_ERROR (errcode);
 
-    /* Should we do something here? */
+        /* Should we do something here? */
 
-    OCL_CHECK_ERROR (clEnqueueUnmapMemObject (app->queue, buffer, array, 0, NULL, NULL));
-    g_timer_stop (app->upload);
+        OCL_CHECK_ERROR (clEnqueueUnmapMemObject (app->queue, buffer, array, 0, NULL, NULL));
+        g_timer_stop (timer);
 
-    /* Call a stupid kernel to avoid any optimizing assumptions of the OpenCL
-     * run-time */
+        *upload += g_timer_elapsed (timer, NULL);
 
-    OCL_CHECK_ERROR (clSetKernelArg (app->kernel, 0, sizeof (cl_mem), &buffer));
-    OCL_CHECK_ERROR (clEnqueueNDRangeKernel (app->queue, app->kernel, 1, NULL, &size, NULL, 0, NULL, &event));
-    OCL_CHECK_ERROR (clWaitForEvents (1, &event));
-    OCL_CHECK_ERROR (clReleaseEvent (event));
+        /* Call a stupid kernel to avoid any optimizing assumptions of the OpenCL
+         * run-time */
 
-    g_timer_start (app->download);
-    array = clEnqueueMapBuffer (app->queue, buffer, CL_TRUE, CL_MAP_READ, 0, size, 0, NULL, NULL, &errcode);
-    OCL_CHECK_ERROR (errcode);
+        OCL_CHECK_ERROR (clSetKernelArg (app->kernel, 0, sizeof (cl_mem), &buffer));
+        OCL_CHECK_ERROR (clEnqueueNDRangeKernel (app->queue, app->kernel, 1, NULL, &size, NULL, 0, NULL, &event));
+        OCL_CHECK_ERROR (clWaitForEvents (1, &event));
+        OCL_CHECK_ERROR (clReleaseEvent (event));
 
-    /* Should we do something here? */
+        g_timer_start (timer);
+        array = clEnqueueMapBuffer (app->queue, buffer, CL_TRUE, CL_MAP_READ, 0, size, 0, NULL, NULL, &errcode);
+        OCL_CHECK_ERROR (errcode);
 
-    OCL_CHECK_ERROR (clEnqueueUnmapMemObject (app->queue, buffer, array, 0, NULL, NULL));
-    g_timer_stop (app->download);
+        /* Should we do something here? */
+
+        OCL_CHECK_ERROR (clEnqueueUnmapMemObject (app->queue, buffer, array, 0, NULL, NULL));
+        g_timer_stop (timer);
+        *download += g_timer_elapsed (timer, NULL);
+    }
 
     clReleaseMemObject (buffer);
+    g_timer_destroy (timer);
 }
 
 
@@ -97,22 +112,14 @@ void run (App *app)
 
 #define NORMALIZE(x) (size / 1024. / 1024. / (x) / NUM_RUNS)
 
-    for (size_t size = 4096; size < 256 * 1024 * 1024 + 1; size += 4096) {
+    for (size_t size = 256 * 1024; size < 64 * 1024 * 1024 + 1; size += 256 * 1024) {
         gdouble upload_normal = 0.0;
         gdouble download_normal = 0.0;
         gdouble upload_pinned = 0.0;
         gdouble download_pinned = 0.0;
 
-        for (int i = 0; i < NUM_RUNS; i++) {
-            measure_transfer_normal (app, size);
-
-            upload_normal += g_timer_elapsed (app->upload, NULL);
-            download_normal += g_timer_elapsed (app->download, NULL); 
-
-            measure_transfer_pinned (app, size);
-            upload_pinned += g_timer_elapsed (app->upload, NULL);
-            download_pinned += g_timer_elapsed (app->download, NULL); 
-        }
+        measure_transfer_normal (app, size, &upload_normal, &download_normal);
+        measure_transfer_pinned (app, size, &upload_pinned, &download_pinned);
 
         g_print ("%zu  %.5f  %.5f  %.5f  %.5f\n", size,
                  NORMALIZE (upload_normal), NORMALIZE (download_normal),
@@ -141,13 +148,9 @@ main (void)
     app.kernel = clCreateKernel (program, "touch", &errcode);
     OCL_CHECK_ERROR (errcode);
 
-    app.upload = g_timer_new ();
-    app.download = g_timer_new ();
+    app.num_runs = 3;
 
     run (&app);
-
-    g_timer_destroy (app.upload);
-    g_timer_destroy (app.download);
 
     clReleaseKernel (app.kernel);
     clReleaseProgram (program);
