04037e3fc9fca83482f3546559f6dcd15ef27249
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-06-14 14:15:23 +0200

    Add perf-precision

---
+++ examples/perf-precision.c
@@ -0,0 +1,23 @@
+#include <ocl.h>
+#include <stdio.h>
+
+int
+main (int argc, const char **argv)
+{
+    OclPlatform *ocl;
+    cl_device_id *devices;
+
+    ocl = ocl_new_from_args (argc, argv, 0);
+    devices = ocl_get_devices (ocl);
+
+    for (int i = 0; i < ocl_get_num_devices (ocl); i++) {
+        static char name[256];
+        size_t resolution;
+
+        OCL_CHECK_ERROR (clGetDeviceInfo (devices[i], CL_DEVICE_NAME, 256, name, NULL));
+        OCL_CHECK_ERROR (clGetDeviceInfo (devices[i], CL_DEVICE_PROFILING_TIMER_RESOLUTION, sizeof(size_t), &resolution, NULL));
+        printf ("\"%s\" %zu\n", name, resolution);
+    }
+
+    ocl_free (ocl);
+}
