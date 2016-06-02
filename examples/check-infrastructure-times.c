90b0676b64ae6111a6bc8173a68ac27d279f7916
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2016-06-02 10:28:00 +0200

    Better output for infrastructure times

--- examples/check-infrastructure-times.c
+++ examples/check-infrastructure-times.c
@@ -18,7 +18,7 @@ main (void)
     timer = g_timer_new ();
     ocl = ocl_new_with_queues (0, CL_DEVICE_TYPE_ALL, 0);
     g_timer_stop (timer);
-    g_print ("Creating context with devices and queues: %fs\n", g_timer_elapsed (timer, NULL));
+    g_print ("Create context: %8.6f s\n", g_timer_elapsed (timer, NULL));
 
     if (ocl == NULL)
         return 1;
@@ -27,7 +27,7 @@ main (void)
     program = ocl_create_program_from_file (ocl, "test.cl", NULL, &errcode);
     OCL_CHECK_ERROR (errcode);
     g_timer_stop (timer);
-    g_print ("Build program: %fs\n", g_timer_elapsed (timer, NULL));
+    g_print ("Build program : %8.6f s\n", g_timer_elapsed (timer, NULL));
 
     cmd_queues = ocl_get_cmd_queues (ocl);
 
@@ -35,7 +35,7 @@ main (void)
     kernel = clCreateKernel (program, "fill_ones", &errcode);
     OCL_CHECK_ERROR (errcode);
     g_timer_stop (timer);
-    g_print ("Create kernel: %fs\n", g_timer_elapsed (timer, NULL));
+    g_print ("Create kernel : %8.6f s\n", g_timer_elapsed (timer, NULL));
 
     g_timer_start (timer);
     n_elements = 1024 * 1024;
@@ -43,7 +43,7 @@ main (void)
                           n_elements * sizeof (float),
                           NULL, &errcode);
     g_timer_stop (timer);
-    g_print ("Create buffer: %fs\n", g_timer_elapsed (timer, NULL));
+    g_print ("Create buffer : %8.6f s\n", g_timer_elapsed (timer, NULL));
 
     OCL_CHECK_ERROR (clSetKernelArg (kernel, 0, sizeof (cl_mem), &mem));
     OCL_CHECK_ERROR (clEnqueueNDRangeKernel (cmd_queues[0], kernel,
@@ -59,7 +59,7 @@ main (void)
     OCL_CHECK_ERROR (clReleaseProgram (program));
     ocl_free (ocl);
     g_timer_stop (timer);
-    g_print ("Cleanup: %fs\n", g_timer_elapsed (timer, NULL));
+    g_print ("Cleanup       : %8.6f s\n", g_timer_elapsed (timer, NULL));
 
     return 0;
 }
